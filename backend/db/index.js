aeaa78d97a1c288658057eae41f03200b8ab54eb
Om Prakash Pati <oppati04@gmail.com>
2026-03-22 16:44:12 +0530

    db setup

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
+import pkg from 'pg';
 
-const client = new Client({
+const { Pool } = pkg;
+
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
-const db = drizzle(client);
+const db = drizzle(pool);
 
 export default db;
